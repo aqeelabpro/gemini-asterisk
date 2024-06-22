7841ee077abfa53cb808eac892abdc3e3034e180
sadiqhussain-dev <hussainosadiq@gmail.com>
2024-06-22 13:45:21 +0500

    create new branch

--- src/main/java/com/google/GeminiAsteriskApplication.java
+++ src/main/java/com/google/GeminiAsteriskApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AmazonPollyAsteriskApplication {
+public class GeminiAsteriskApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AmazonPollyAsteriskApplication.class, args);
+        SpringApplication.run(GeminiAsteriskApplication.class, args);
     }
 
 }
